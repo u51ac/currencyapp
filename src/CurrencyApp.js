dc34e44c6ee2c6e81c3e8a2fef5158c2fcb1a1e1
don <ddondongg@gmail.com>
2018-03-01 08:24:28 -0800

    项目组织架构

---
+++ src/CurrencyApp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class CurrencyApp extends React.Component {
+    render() {
+        return(
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Hello World!</Text>
+            </View>
+        );
+    }
+}  
+
+ 
+
+
+
+
---
+++ src/components/Button/ButtonWithInput.js
@@ -0,0 +1,3 @@
+import { View } from 'react-native';
+
+const ButtonWithInput = () => <View></View>;
